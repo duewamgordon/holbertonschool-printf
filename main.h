e24740a85eea19c11966973c11d74815218d0429
duewamgordon <duewamgordon@gmail.com>
2023-03-13 21:35:30 -0700

    printf

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+typedef struct ptr
+{
+   	char *c;
+   	int (*f)(char *, int, va_list);
+}
+pt_char;
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
