634b71f1da6571be01ebc3bdea62f6a2773b55f9
root <duewamgordon@gmail.com>
2023-07-10 14:04:52 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define 
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +7,13 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <limits.h>
+
+/**STRUCTURE*/
+/**
+ * struct func - Structure of the format given to printf
+ * @mod: The format specifier given
+ * @f: The function that will be called
+ */
 
 int _printf(const char *format, ...);
 
